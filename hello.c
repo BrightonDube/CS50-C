804a37bc9ef330bc3f9339d8ee35dac46a40a636
BrightonDube <BrightonDube@users.noreply.github.com>
2022-01-15 18:16:35 +0200

    add cs50 library

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
+#include <cs50.h>
 #include <stdio.h>
-int main (void) {
-	printf("Hello World\n");
-}
 
+int main(void)
+{
+	string name = get_string("What's your name? ");
+	printf("Hello %s\n", name);
+	string age = get_string("How old are you? ");
+	printf("You are %s years old \n", age);
+}
